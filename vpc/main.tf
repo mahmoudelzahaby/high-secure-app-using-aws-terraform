d451b8500933e61f1e94104637696eafb3c55cd9
MahmoudDahaby <99130650+mahmouddahaby@users.noreply.github.com>
2023-02-07 14:59:15 +0200

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -10,6 +10,7 @@ resource "aws_subnet" "subnets" {
   count = length(var.subnet-cidr)
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.subnet-cidr[count.index]
+  availability_zone = var.az-subnet[count.index]
 }
 
 
