114dc77993f3723c4f2fd1e8ae3505e7889ce21c
MahmoudDahaby <99130650+mahmouddahaby@users.noreply.github.com>
2023-02-07 15:02:28 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,5 +3,5 @@ module "vpc" {
   cidr-vpc    = "10.0.0.0/16"
   name-vpc    = "vpc-iti-1"
   subnet-cidr = ["10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
-  az-subnet = ["us-east-2a","us-east-2a","us-east-2b","us-east-2b"]
+  az-subnet = ["us-east-2a","us-east-2b","us-east-2a","us-east-2b"]
 }
